1290e80e3f2043fc2984d66a3d669875254e0cec
Eliott Nys <eliott.nys@gmail.com>
2023-03-16 14:30:43 +0100

    added lead-lag real time

--- package_LAB.py
+++ package_LAB.py
@@ -1,11 +1,12 @@
-def FO_RT(MV,Kp,T,Ts,PV,PVInit=0,method='EBD'):
+def LL_RT(MV,Kp,Tlead,Tlag,Ts,PV,PVInit=0,method='EBD'):
     
     """
-    The function "FO_RT" needs to be included in a "for or while loop".
+    The function "LL_RT" needs to be included in a "for or while loop".
     
     :MV: input vector
     :Kp: process gain
-    :T: lag time constant [s]
+    :Tlead: lead time constant [s]
+    :Tlag: lag time constant [s]
     :Ts: sampling period [s]
     :PV: output vector
     :PVInit: (optional: default value is 0)
@@ -14,26 +15,26 @@ def FO_RT(MV,Kp,T,Ts,PV,PVInit=0,method='EBD'):
         EFD: Euler Forward difference
         TRAP: Trapezoïdal method
     
-    The function "FO_RT" appends a value to the output vector "PV".
+    "LL_RT" = Lead-lag real-time
+    The function "LL_RT" appends a value to the output vector "PV".
     The appended value is obtained from a recurrent equation that depends on the discretisation method.
     """    
-    
-    if (T != 0):
-        K = Ts/T
+
+    if (Tlag != 0):
+        K = Ts/Tlag
         if len(PV) == 0:
             PV.append(PVInit)
         else: # MV[k+1] is MV[-1] and MV[k] is MV[-2]
             if method == 'EBD':
-                PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*MV[-1])
+                PV.append((1 / (1 + K)) * PV[-1] + ((Kp * K) / (1 + K)) * ((1 + Tlead / Ts) * MV[-1] - (Tlead / Ts) * MV[-2]))
             elif method == 'EFD':
-                PV.append((1-K)*PV[-1] + K*Kp*MV[-2])
-            elif method == 'TRAP':
-                PV.append((1/(2*T+Ts))*((2*T-Ts)*PV[-1] + Kp*Ts*(MV[-1] + MV[-2])))            
-            else:
-                PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*MV[-1])
+                PV.append((1 - K) * PV[-1] + Kp * K * ((Tlead / Ts) * MV[-1] + (1 - Tlead / Ts) * MV[-2]))
+            elif method == 'TRAP':  #the equation is not in the slides
+                PV.append(0)      
+            else:   #if typo -> EBD (default)
+                PV.append((1 / (1 + K)) * PV[-1] + Kp * K * ((Tlead * MV[-1]) / Ts) + (1 - Tlead / Ts) * MV[-2])
     else:
         PV.append(Kp*MV[-1])
-
 
 def PID_RT(MV,Kp,T,Ts,PV,PVInit=0,method='EBD'):
     
